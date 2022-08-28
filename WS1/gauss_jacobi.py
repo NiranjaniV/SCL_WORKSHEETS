c83580f83951d48079c2e73a5066d36a95cc7590
NiranjaniV <76519972+NiranjaniV@users.noreply.github.com>
2022-08-28 22:08:55 +0530

    Update gauss_jacobi.py

--- WS1/gauss_jacobi.py
+++ WS1/gauss_jacobi.py
@@ -41,3 +41,40 @@ print("B: ")
 pprint(b)
 print("x = ")
 pprint(sol)
+
+-------------------------------
+def f1(x,y,z):
+    return (2-y-z)/4
+def f2(x,y,z):
+    return (-6-x-2*z)/5
+def f3(x,y,z):
+    return (-4-x-2*y)/3
+
+
+x0 = 0
+y0 = 0
+z0 = 0
+
+count =1
+
+e = float(input("Enter the tolerable error"))
+condition = True
+
+while condition:
+    x1 = f1(x0,y0,z0)
+    y1 = f2(x0,y0,z0)
+    z1 = f3(x0,y0,z0)
+    
+    print("%d\t%0.4f\t%0.4f\t%0.4f\t",(count,x1,y1,z1))
+    e1 = abs(x1-x0)
+    e2 = abs(y1-y0)
+    e3 = abs(z1-z0)
+    
+    count+=1
+    x0=x1
+    y0=y1
+    z0=z1
+    
+    condition = e1>e and e2>e and e3>e
+    
+    print('\nSolution: x=%0.3f, y=%0.3f and z = %0.3f\n'% (x1,y1,z1))
