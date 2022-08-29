0a675f56f627e7b3b51b0a191c6870877ac6b70e
NiranjaniV <76519972+NiranjaniV@users.noreply.github.com>
2022-08-29 13:10:55 +0530

    Update newton_raphson.py

--- WS3/newton_raphson.py
+++ WS3/newton_raphson.py
@@ -14,3 +14,17 @@ def newtonRaphson( x ):
  
 x0 = 0.1 # Initial value
 newtonRaphson(x0)
+
+------------------------------------------
+def newton_raphson(f, limit):
+    a = limit[0]
+    b = limit[1]
+    t = float(a + b) / 2
+
+    while f.subs(x, t) > 0.001 or f.subs(x, t) < -0.001:
+        print("t =", t, "\nf(t) =", f.subs(x, t), end = "\n\n")
+        t = t - f.subs(x, t) / diff(f, x).subs(x, t)
+
+    print("SOLUTION\nx = {x}\nf(x) = {fx}".format(x = t, fx = f.subs(x, t)))
+
+newton_raphson(f,limit)
